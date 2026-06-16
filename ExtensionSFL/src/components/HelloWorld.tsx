db02e96b2de57b45f5acca88ebe3cc9537b1d05d
Andrey19978 <andros.g@mail.ru>
2026-06-16 21:49:27 +0300

    Button Stop

--- ExtensionSFL/src/components/HelloWorld.tsx
+++ ExtensionSFL/src/components/HelloWorld.tsx
@@ -8,9 +8,19 @@ export default function HelloWorld() {
     });
   }
 
+    function offUserClickedButton() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const tabId = tabs[0]?.id;
+      if (tabId) {
+        chrome.tabs.sendMessage(tabId, { command: 'stop' });
+      }
+    });
+  }
+
   return (
     <>
       <button onClick={onUserClickedButton}>Click me</button>
+      <button onClick={offUserClickedButton}>Stop me</button>
     </>
   );
 }
--- ExtensionSFL/src/content/main.tsx
+++ ExtensionSFL/src/content/main.tsx
@@ -5,35 +5,66 @@ import App from './views/App.tsx'
 console.log('Расшерение подключенно')
 
 // content-script.js
-
+let timer: any = null;
 // --- Шаг А: Объявляем функцию (назвали "doClick") ---
 function doClick() {
-    console.log('Выполняю клик!');
-    const element = document.querySelector("#game-board > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(147) > div > div > div");
-    
-    if (element) {
-        (element as HTMLElement).click();
-        console.log('Клик выполнен успешно!');
-    } else {
-        console.log('Элемент не найден!');
+    function innerCollect() {
+        chrome.storage.local.get("abort", (result) => {
+            if (result.abort) return;
+
+            const elements = [];
+            for (let i = 1; i <= 215; i++) {
+                const selector = `#game-board > div.absolute.w-full.h-full.z-10 > div > div.absolute.left-1\\/2.top-1\\/2.-translate-x-1\\/2.-translate-y-1\\/2 > div > div:nth-child(${i}) > div > div > div`;
+                elements.push(document.querySelector(selector));
+            }
+
+            elements.forEach((element) => {
+                if (element) {
+                    for (let i = 0; i < 3; i++) {
+                        element.click();
+                    }
+                }
+            });
+
+            if (!result.abort) {
+                timer = setTimeout(innerCollect, 5000);
+            }
+        });
     }
+
+    innerCollect()
 }
+
 
 // --- Шаг Б: Ждем команду "голоса" ---
 chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
     if (request.command === 'клик') {   // если голос сказал "клик"
-        doClick();                       // вызываем нашу функцию
+        doClick();                      // вызываем нашу функцию
         sendResponse({ status: 'клик выполнен' });
     }
 
-return true
-  });
+    return true
+});
+
+function stopSle() {
+    clearTimeout(timer);
+    timer = null;
+}
+
+chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+    if (request.command === 'stop') {   // если голос сказал "клик"
+        stopSle()        // вызываем нашу функцию
+        sendResponse({ status: 'клик выполнен' });
+    }
+
+    return true
+});
 
 const container = document.createElement('div')
 container.id = 'crxjs-app'
 document.body.appendChild(container)
 createRoot(container).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
